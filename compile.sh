bbc4d7b4aca95d86ea30c7ffebef3861b95ab1a7
diegobnh <diegobnh@gmail.com>
2021-10-31 17:12:50 -0400

    auto commit

--- compile.sh
+++ compile.sh
@@ -28,7 +28,7 @@ LD_PRELOAD=./mmap_intercept.so /scratch/gapbs/./bc -f /scratch/gapbs/benchmark/g
 
 #./main 1> /dev/null &
 pid_main=$!
-taskset -cp 0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34 $pid_main
+taskset -cp 0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34 $pid_main 1> /dev/null
 #LD_PRELOAD=./malloc_intercept.so ./main 1> /dev/null
 
 
