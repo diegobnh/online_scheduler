9176a751ca789656853b8286fe71238cdd4b88c3
diegobnh <diegobnh@gmail.com>
2021-11-02 09:54:42 -0400

    auto commit

--- actuator.c
+++ actuator.c
@@ -65,7 +65,7 @@ void sort_objects(struct schedule_manager *args){
     }
     for(i=0;i<args->tier[1].num_obj;i++){
         for(j=i+1;j<args->tier[1].num_obj;j++){
-            if(args->tier[1].obj_vector[i].metrics.loads_count[4] > args->tier[1].obj_vector[j].metrics.loads_count[4]){
+            if(args->tier[1].obj_vector[i].metrics.loads_count[4] < args->tier[1].obj_vector[j].metrics.loads_count[4]){
                 aux = args->tier[1].obj_vector[j];
                 args->tier[1].obj_vector[j] = args->tier[1].obj_vector[i];
                 args->tier[1].obj_vector[i] = aux;
