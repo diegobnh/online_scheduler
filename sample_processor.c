a720e28c731b2b6445e730e6394f22485c0a384d
diegobnh <diegobnh@gmail.com>
2021-11-04 22:46:13 -0400

    auto commit

--- sample_processor.c
+++ sample_processor.c
@@ -82,10 +82,31 @@ int calculate_SMA_for_PMEM(void){
 		}
         g_pmem_metrics[i].stores_count = (double)stores/RING_BUFFER_SIZE;
 	}
+    print_struc_pmem(g_pmem_tier_ring)
 }
 
+void print_struc_pmem(ring_buffer_t * g_dram_tier_ring){
+    int i, j, w;
 
-void print_struct(ring_buffer_t * g_dram_tier_ring){
+    for(i=0; i< g_total_pmem_objs; i++){
+        fprintf(stderr, "i=%d\n",i);
+        for(w=0; w< MEM_LEVELS; w++){
+            fprintf(stderr, "\tw=%d\n",w);
+            for(j=0; j< RING_BUFFER_SIZE; j++){
+                fprintf(stderr, "\t\tj=%d, ",j);
+                fprintf(stderr, "\t\t\tlat:%ld, \
+                                 load:%ld, \
+                                 tlb_miss:%ld,\
+                                 tlb_hit:%ld\n", \
+                                 g_pmem_tier_ring[i].sum_latency_cost[j][w],\
+                                 g_pmem_tier_ring[i].loads_count[j][w],\
+                                 g_pmem_tier_ring[i].TLB_hit[j][w],\
+                                 g_pmem_tier_ring[i].TLB_miss[j][w]);
+            }
+        }
+    }
+}
+void print_struc_dram(ring_buffer_t * g_dram_tier_ring){
     int i, j, w;
 
     for(i=0; i< g_total_dram_objs; i++){
