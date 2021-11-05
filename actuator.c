8f10a4e42870e64e7ce1c36dd6f5b06bcb242da6
diegobnh <diegobnh@gmail.com>
2021-11-04 22:09:56 -0400

    auto commit

--- actuator.c
+++ actuator.c
@@ -80,16 +80,16 @@ void check_candidates_to_migration(struct schedule_manager *args){
     static int num_call = 0;
     fprintf(stderr, "%d\n", num_call);
     for(i=0;i<args->tier[0].num_obj;i++){
-        if(args->tier[0].obj_vector[i].metrics.loads_count[4] != 0 && \
-           args->tier[0].obj_flag_alloc[i] == 1){
+        //if(args->tier[0].obj_vector[i].metrics.loads_count[4] != 0 &&
+        if(args->tier[0].obj_flag_alloc[i] == 1){
             fprintf(stderr, "DRAM[%d,%lu] = %.2lf\n", i, args->tier[0].obj_vector[i].size, args->tier[0].obj_vector[i].metrics.loads_count[4]);
         }
         
     }
     //fprintf(stderr, "###\n");
     for(i=0;i<args->tier[1].num_obj;i++){
-        if(args->tier[1].obj_vector[i].metrics.loads_count[4] != 0 && \
-           args->tier[1].obj_flag_alloc[i] == 1){
+        //if(args->tier[1].obj_vector[i].metrics.loads_count[4] != 0 &&
+        if(args->tier[1].obj_flag_alloc[i] == 1){
             fprintf(stderr, "PMEM[%d,%lu] = %.2lf\n", i, args->tier[1].obj_vector[i].size, args->tier[1].obj_vector[i].metrics.loads_count[4]);
         }
         
@@ -109,7 +109,7 @@ void *thread_actuator(void *_args){
        
        pthread_mutex_lock(&args->global_mutex);
        sort_objects(args);
-       //check_candidates_to_migration(args);
+       check_candidates_to_migration(args);
        pthread_mutex_unlock(&args->global_mutex);
        /*
        pthread_mutex_lock(&args->global_mutex);
