61a324a6af39b333dd9e4ec9534c89bb73e8e0f2
diegobnh <diegobnh@gmail.com>
2021-11-13 18:39:54 -0500

    auto commit

--- mmap_intercept.c
+++ mmap_intercept.c
@@ -45,7 +45,7 @@
 #endif
 
 
-#define MAXIMUM_DRAM_CAPACITY 1000000000  //means 4GB
+#define MAXIMUM_DRAM_CAPACITY 99000000000  //means 4GB
 
 //#define DEBUG
 #ifdef DEBUG
@@ -144,8 +144,8 @@ hook(long syscall_number, long arg0, long arg1,	long arg2, long arg3, long arg4,
 #elif INIT_ALLOC == RANDOM
         if(rand() % 2){
 #elif FIRST_DRAM
-        //if(1){
-        if(0){
+        if(1){
+        //if(0){
 #endif
 		   if((unsigned long)arg1 + mem_consumption < MAXIMUM_DRAM_CAPACITY){
                
