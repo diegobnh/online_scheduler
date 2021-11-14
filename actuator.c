b4ccc9e7f279a754cbd65617ce289fe71dabdc12
diegobnh <diegobnh@gmail.com>
2021-11-14 18:27:57 -0500

    auto commit

--- actuator.c
+++ actuator.c
@@ -90,7 +90,7 @@ void check_candidates_to_migration(struct schedule_manager *args){
     fprintf(stderr, "---\n");
     for(i=0;i<args->tier[1].num_obj;i++){
         if(args->tier[1].obj_vector[i].metrics.loads_count[4] != 0 && args->tier[1].obj_flag_alloc[i] == 1){
-            fprintf(stderr, "DRAM[%d,%lu] = %.2lf\n", i, args->tier[1].obj_vector[i].size, args->tier[1].obj_vector[i].metrics.loads_count[4]);
+            fprintf(stderr, "PMEM[%d,%lu] = %.2lf\n", i, args->tier[1].obj_vector[i].size, args->tier[1].obj_vector[i].metrics.loads_count[4]);
         }
         /*
         if(args->tier[1].obj_vector[i].metrics.loads_count[4] != 0 && args->tier[1].obj_flag_alloc[i] == 1){
