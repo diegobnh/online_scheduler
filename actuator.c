a5fa7e4b44901eb67b7fab570fa5cbe6715d9f8a
diegobnh <diegobnh@gmail.com>
2021-11-15 10:24:00 -0500

    auto commit

--- actuator.c
+++ actuator.c
@@ -122,7 +122,7 @@ void check_candidates_to_migration(struct schedule_manager *args){
     fprintf(stderr, "\nCurrent PMEM consumption:%ld", args->tier[1].current_memory_consumption);
     fprintf(stderr, "\nCurrent DRAM space:%ld (bytes) , %.4lf(GB)", \
             (MAXIMUM_DRAM_CAPACITY - args->tier[0].current_memory_consumption),\
-            (MAXIMUM_DRAM_CAPACITY - args->tier[0].current_memory_consumption)/1000000000);
+            (MAXIMUM_DRAM_CAPACITY - args->tier[0].current_memory_consumption)/1000000000.0);
     fprintf(stderr, "\n-------------------------------------\n");
     
 }
