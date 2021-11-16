c54a7549edef6143f341b3eeb1c87a01306fba18
diegobnh <diegobnh@gmail.com>
2021-11-16 09:46:55 -0500

    auto commit

--- actuator.c
+++ actuator.c
@@ -83,7 +83,7 @@ int check_candidates_to_migration(struct schedule_manager *args){
             }else{
                 fprintf(stderr, "DRAM[%d,%.4lf] = %04.2lf read-only\n", i, args->tier[0].obj_vector[i].size/GB, args->tier[0].obj_vector[i].metrics.loads_count[4]/(args->tier[0].obj_vector[i].size/GB));
             }
-            flag_has_llcm = 1;
+            //flag_has_llcm = 1;
         }
         
     }
@@ -106,7 +106,7 @@ int check_candidates_to_migration(struct schedule_manager *args){
     return flag_has_llcm;
 }
 
-void policy_migration_upgrade(struct schedule_manager *args){
+void policy_migration_promotion(struct schedule_manager *args){
     int i;
     float current_dram_space;
     unsigned long nodemask;
@@ -148,10 +148,10 @@ void policy_migration_upgrade(struct schedule_manager *args){
             
         }
     }
-    fprintf(stderr, "\nNum obj upgraded:%d\n", num_obj_migrated);
+    fprintf(stderr, "\nNum obj promotiond:%d\n", num_obj_migrated);
     
 }
-int policy_migration_downgrade(struct schedule_manager *args){
+int policy_migration_demotion(struct schedule_manager *args){
     int i;
     float current_dram_space;
     unsigned long nodemask;
@@ -166,7 +166,7 @@ int policy_migration_downgrade(struct schedule_manager *args){
     
     nodemask = 1<<NODE_0_PMEM;
     
-    //First get the top1 from pmem to upgrade in the next round
+    //First get the top1 from pmem to promotion in the next round - get candidate
     for(i=0;i<args->tier[1].num_obj;i++){
         if(args->tier[1].obj_vector[i].metrics.loads_count[4] != 0 && args->tier[1].obj_flag_alloc[i] == 1){
             top1_pmem_llcm = args->tier[1].obj_vector[i].metrics.loads_count[4];
@@ -205,14 +205,16 @@ int policy_migration_downgrade(struct schedule_manager *args){
                 }
                 
                 top1_pmem_size -= args->tier[0].obj_vector[i].size/GB;
-                if(top1_pmem_size < 0){
+                if(top1_pmem_size <= 0){
                     break;
                 }
+            }else{
+                break;
             }
         }
         
     }
-    fprintf(stderr, "Num obj downgraded:%d\n", num_obj_migrated);
+    fprintf(stderr, "Num obj demotiond:%d\n", num_obj_migrated);
     fprintf(stderr, "-------------------------------------\n");
 }
 
@@ -220,6 +222,7 @@ void *thread_actuator(void *_args){
     struct schedule_manager *args = (struct schedule_manager *) _args;
     int i,j;
     int flag_has_llcm;
+    float current_dram_space;
     
     while(1){
        sleep(SLEEP_TIME);
@@ -228,19 +231,17 @@ void *thread_actuator(void *_args){
        unsigned long nodemask = 1<<NODE_1_DRAM;
        
        pthread_mutex_lock(&args->global_mutex);
-
        sort_objects(args);
        flag_has_llcm = check_candidates_to_migration(args);
-         
+       current_dram_space = (MAXIMUM_DRAM_CAPACITY - args->tier[0].current_memory_consumption)/GB;
        pthread_mutex_unlock(&args->global_mutex);
 
-       if(flag_has_llcm == 1)  {
-           policy_migration_upgrade(args);//move top objects from PMEM to DRAM
-           policy_migration_downgrade(args);//move non-top objetcts from DRAM to PMEM
+       if(flag_has_llcm == 1 && current_dram_space > 0)  {
+           policy_migration_promotion(args);//move top objects from PMEM to DRAM
        }
-        
-       
-       
+       else if(current_dram_space <= 0){
+           policy_migration_demotion(args);//move non-top objetcts from DRAM to PMEM
+       }
     }//while end
 }
 
