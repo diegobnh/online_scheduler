ec1d9df59bcf61b4869ff2620533cf6a365c799b
diegobnh <diegobnh@gmail.com>
2021-11-20 15:41:12 -0500

    auto commit

--- actuator.c
+++ actuator.c
@@ -232,7 +232,7 @@ int policy_migration_demotion(struct schedule_manager *args){
     }
     fprintf(stderr,"Somatorio dos LLCM:%.2lf, PMEM candidate:%.2lf\n", sum_llcm_candidates_demotion, top1_pmem_llcm);
     
-    
+    /*
     cont = 0;
     if(sum_llcm_candidates_demotion < top1_pmem_llcm){
         while(list_obj_index[cont] != -1){
@@ -265,14 +265,15 @@ int policy_migration_demotion(struct schedule_manager *args){
     }else{
         fprintf(stderr,"Sum of all objs from DRAM has more LLCM (%.2lf) than Top1 from PMEM (%.2lf)!!\n",sum_llcm_candidates_demotion, top1_pmem_llcm);
     }
-    
+    */
     
     
     //Try to remove N objects from DRAM
-    /*
+    
     for(i=args->tier[0].num_obj-1; i >= 0; i--){
         curr_llcm = args->tier[0].obj_vector[i].metrics.loads_count[4]/(args->tier[0].obj_vector[i].size/GB);
-        if(curr_llcm > 1 && args->tier[0].obj_flag_alloc[i] == 1){
+        //if(curr_llcm > 1 && args->tier[0].obj_flag_alloc[i] == 1){
+        if(args->tier[0].obj_flag_alloc[i] == 1){
             fprintf(stderr, "Checking if Coldest DRAM (%.2lf) <  Hottest PMEM (%.2lf)\n", curr_llcm,top1_pmem_llcm);
             if(curr_llcm < top1_pmem_llcm){
                 
