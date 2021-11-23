a0552d387351f433d1b77af4687f43b4a8702e20
diegobnh <diegobnh@gmail.com>
2021-11-23 05:33:10 -0500

    auto commit

--- compile.sh
+++ compile.sh
@@ -12,13 +12,13 @@ gcc -g -I/scratch/build/autonuma-r6/tools/lib/perf/include/ -o monitor monitor_v
 gcc -g -fno-pie mmap_intercept.c -rdynamic -fpic -shared -o mmap_intercept.so recorder.o actuator.o -lpthread -lrt -lnuma -lsyscall_intercept -DINIT_ALLOC=4;
 
 #In case the application in the last execution has finished without remove shared memory
-sudo ./delete_shared_memory
+./delete_shared_memory
 
 #SECONDS=0
-sudo LD_PRELOAD=./mmap_intercept.so /scratch/gapbs/./bc -f /scratch/gapbs/benchmark/graphs/kron.sg -n1 1> /dev/null &
+LD_PRELOAD=./mmap_intercept.so /scratch/gapbs/./bc -f /scratch/gapbs/benchmark/graphs/kron.sg -n1 1> /dev/null &
 pid_app=$!
 
-sudo taskset -cp 0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34  $pid_app 1> /dev/null
+taskset -cp 0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34  $pid_app 1> /dev/null
 
 sleep 1
 sudo ./monitor &
