401aab050fbb1f0d156469d7b5065a605c11b9d4
diegobnh <diegobnh@gmail.com>
2021-11-23 12:50:41 -0500

    auto commit

--- actuator.c
+++ actuator.c
@@ -33,7 +33,8 @@ int g_iteration=0;
 
 int move_pages_function(int pid, unsigned long int addr, unsigned long int size)
 {
-
+    int status_vector[4] = {0,0,0,0};
+    
     if ((numa_available() < 0)) {
         fprintf(stderr, "error: not a numa machine\n");
         return -1;
@@ -84,8 +85,17 @@ int move_pages_function(int pid, unsigned long int addr, unsigned long int size)
     }
     
     for (int i = 0; i < page_count; i++) {
-        fprintf(stderr, "%d\n",status[i]);
+        //fprintf(stderr, "%d\n",status[i]);
+        if(status[i] >= 0){
+            status_vector[status[i]]++;//0,1,2
+        }else{
+            status_vector[3]++;
+        }
     }
+    for(i=0; i<4; i++){
+        fprintf(stderr, "[%d] = %.2lf, ", i, (float)status_vector[status[i]]/page_count);
+    }
+    fprintf(sdterr, "\n");
 
     return 0;
 }
