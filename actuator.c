d7f671fa6ebe27cf368f9bf2374de8e27718b00a
diegobnh <37424347+diegobnh@users.noreply.github.com>
2022-11-13 16:11:58 -0300

    Update actuator.c

--- actuator.c
+++ actuator.c
@@ -20,25 +20,6 @@
 #include "monitor.h"
 #include "recorder.h"
 
-#define NODE_0_DRAM 0
-#define NODE_0_PMEM 2
-#define NODE_1_DRAM 1
-#define NODE_1_PMEM 3
-
-#define ROUND_ROBIN 1
-#define RANDOM 2
-#define FIRST_DRAM 3
-#define BASED_ON_SIZE 4
-
-#define METRIC_ABS_LLCM 1
-#define METRIC_LLCM_PER_SIZE 2
-#define METRIC_ABS_TLB_MISS 3
-#define METRIC_TLB_MISS_PER_SIZE 4
-#define METRIC_ABS_WRITE 5
-#define METRIC_WRITE_PER_SIZE 6
-#define METRIC_ABS_LATENCY 7
-#define METRIC_LATENCY_PER_SIZE 8
-
 #ifndef INIT_DATAPLACEMENT
 #error "INIT_DATAPLACEMENT is not defined."
 #endif
