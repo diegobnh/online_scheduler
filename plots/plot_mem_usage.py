577af9916e36e2c8603adf9e2a19f834c226bb2d
diegobnh <37424347+diegobnh@users.noreply.github.com>
2022-11-21 15:23:37 -0300

    Update plot_mem_usage.py

--- plots/plot_mem_usage.py
+++ plots/plot_mem_usage.py
@@ -11,14 +11,14 @@ def plot_memory_usage():
     name = files[0].split('.')[0]
     app_dataset = name.split('_')[-2] + "_" + name.split('_')[-1]
 
+    #dataframe of memory consumption in DRAM and NVM
     df = pd.read_csv(files[0])
     exec_time = round(df['timestamp'].iloc[-1] - df['timestamp'].iloc[0],1)
     df.set_index('timestamp', inplace=True)
-
     df['dram_app'] = df['dram_app']/1000
-    df['pmem_app'] = df['pmem_app']/1000
+    df['nvm_app'] = df['nvm_app']/1000
 
-
+    #dataframe of samples
     df_external_access = pd.read_csv("loads.txt", names=["ts_event", "virt_addr", "mem_access"])
     df_external_access['ts_event'] = df_external_access['ts_event'].astype(int)
     df_dram = df_external_access.loc[df_external_access.mem_access == "DRAM_hit"]    
@@ -28,16 +28,18 @@ def plot_memory_usage():
     df_nvm = df_nvm.groupby(['ts_event']).size().reset_index(name='NVM')
     df_nvm.set_index("ts_event", inplace=True)
 
+    #Plot
     fig, axes = plt.subplots(figsize=(5,4),nrows=2,sharex=True)
-
+    
+    #First subplot
     df[["dram_app"]].plot(linewidth=0.75, ax=axes[0])
-    df[["pmem_app"]].plot(style="-.",linewidth=0.75, ax=axes[0])
+    df[["nvm_app"]].plot(style="-.",linewidth=0.75, ax=axes[0])
     axes[0].legend(['DRAM','NVM'], prop={'size': 10}, ncol=2, fancybox=True, framealpha=0.5, bbox_to_anchor=(0.9, 1.41))
     axes[0].tick_params(axis='x', rotation=45)
     #ax.set_xlabel('Timestamp(seconds)')
     axes[0].set_ylabel('Memory \n Consumption (GB)')
-
-    #ax2 = ax.twinx()
+    
+    #Second subplot
     df_dram["DRAM"].plot(ax=axes[1], linewidth=0.75, color="tab:blue")
     df_nvm["NVM"].plot(ax=axes[1], style="-.",linewidth=0.75, color="tab:orange")
     axes[1].set_ylabel('Number of Load \n Access')
